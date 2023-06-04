018774d1bf5e98fcc1e94cf0db9a4e57d6e5cf3e
LittleZabi <bigbro4564@gmail.com>
2023-06-04 17:34:19 +0500

    new files

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -38,33 +38,35 @@ const HomeView = () => {
         </div>
       </div>
       <div className="page-size home-x3">
-        <p>
-          Welcome to the District Officer Website! Experience the convenience of
-          our online platform, designed exclusively for citizens like you.
-          Submit your applications, letters, and requests directly to the
-          District Officer (DC) with just a few clicks. Say goodbye to long
-          queues and paperwork hassles, as our user-friendly interface
-          streamlines the entire process. Unlock a world of efficiency and
-          transparency as you leverage the power of digital technology. Track
-          the progress of your applications in real-time, receive timely
-          updates, and ensure that your voice is heard. Our platform is built to
-          empower you, providing a seamless channel to connect with the District
-          Officer and access essential services. Join a community of engaged
-          citizens and contribute to the growth and development of your
-          district. Together, we can foster effective governance, enhance
-          communication, and build a brighter future. Embrace the ease and
-          accessibility of our online platform, and let your applications and
-          letters make an impact today!
-        </p>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.158471546285!2d73.02243167495813!3d34.6759496845418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38de75bbe2e01507%3A0x700f4ad9b65581cf!2sDeputy%20Commissioner%20Office%20-%20Battagram!5e0!3m2!1sen!2s!4v1685877311219!5m2!1sen!2s"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
+        <div className="sup0333">
+          <p>
+            Welcome to the District Officer Website! Experience the convenience
+            of our online platform, designed exclusively for citizens like you.
+            Submit your applications, letters, and requests directly to the
+            District Officer (DC) with just a few clicks. Say goodbye to long
+            queues and paperwork hassles, as our user-friendly interface
+            streamlines the entire process. Unlock a world of efficiency and
+            transparency as you leverage the power of digital technology. Track
+            the progress of your applications in real-time, receive timely
+            updates, and ensure that your voice is heard. Our platform is built
+            to empower you, providing a seamless channel to connect with the
+            District Officer and access essential services. Join a community of
+            engaged citizens and contribute to the growth and development of
+            your district. Together, we can foster effective governance, enhance
+            communication, and build a brighter future. Embrace the ease and
+            accessibility of our online platform, and let your applications and
+            letters make an impact today!
+          </p>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.158471546285!2d73.02243167495813!3d34.6759496845418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38de75bbe2e01507%3A0x700f4ad9b65581cf!2sDeputy%20Commissioner%20Office%20-%20Battagram!5e0!3m2!1sen!2s!4v1685877311219!5m2!1sen!2s"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
       <div className="page-size">
-      <ApplicationForm/>
+        <ApplicationForm />
       </div>
     </div>
   );
